fff4e4ebad3a911cd93b04f88cdc441c2022daac
lama-albayomi <lama.albayomi@gmail.com>
2024-04-05 22:59:49 +0300

    Add Spaces

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Range (0, 10)]
+    [SerializeField] private float LetterSpace;
     [SerializeField] private GameObject alf;
+    [SerializeField] private GameObject alfHamza;
     [SerializeField] private GameObject baa;
     [SerializeField] private GameObject taa;
     [SerializeField] private GameObject thaa;
@@ -13,8 +16,11 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject Kaa;
     [SerializeField] private GameObject dal;
     [SerializeField] private GameObject zal;
+    [SerializeField] private GameObject raa;
+    [SerializeField] private GameObject zeen;
+    [SerializeField] private GameObject seen;
     [SerializeField] private Transform startPos;
-    private int numberofLetters = 0;
+    private int numberofSpaces = 0;
     private bool isEnded= false;
     private List<Letter> letters = new List<Letter>();
     void Start(){
@@ -31,55 +37,42 @@ public class GameManager : MonoBehaviour
         string input = Input.inputString;
 
         // Check if the input matches the Arabic character "أ"
-        if (input == "أ"){
+        if (input == "ا"){
             LetterEntered(alf);
             Debug.Log("alf");
-        } else if (input == "ب"){
+        } else if (input == "أ"){
+            LetterEntered(alfHamza);
+        }else if (input == "ب"){
             LetterEntered(baa);
-            Debug.Log("baa");
         }else if (input == "ت"){
             LetterEntered(taa);
-            Debug.Log("baa");
         }else if (input == "ث"){
             LetterEntered(thaa);
-            Debug.Log("baa");
         }else if (input == "ج"){
             LetterEntered(Jeem);
-            Debug.Log("baa");
         }else if (input == "ح"){
             LetterEntered(haa);
-            Debug.Log("baa");
         }else if (input == "خ"){
             LetterEntered(Kaa);
-            Debug.Log("baa");
         }else if (input == "د"){
             LetterEntered(dal); 
-            Debug.Log("baa");
         }else if (input == "ذ"){
             LetterEntered(zal);
-            Debug.Log("baa");
-        }
-        
-        if(Input.GetKeyDown(KeyCode.A)){
-            Debug.Log("A");
-            LetterEntered(alf);
-        }
-        if(Input.GetKeyDown(KeyCode.B)){
-            Debug.Log("B");
-            LetterEntered(baa);
-        }
-        if(Input.GetKeyDown(KeyCode.T)){
-            Debug.Log("T");
-            LetterEntered(taa);
+        }else if (input == "ر"){
+            LetterEntered(raa);
+        }else if (input == "ز"){
+            LetterEntered(zeen);
+        }else if (input == "س"){
+            LetterEntered(seen);
         }
         
         
         
     }
     public void LetterEntered(GameObject letter){
-        numberofLetters++;
         int size = letter.GetComponent<Letter>().size;
-        Vector3 letterPosition = startPos.position + new Vector3(numberofLetters + size, 0, 0);
+        numberofSpaces += size;
+        Vector3 letterPosition = startPos.position + new Vector3(numberofSpaces * LetterSpace, 0, 0);
         Letter inst =  Instantiate(letter, letterPosition, Quaternion.identity).GetComponent<Letter>();
         letters.Add(inst);
         
@@ -100,14 +93,13 @@ public class GameManager : MonoBehaviour
         letters.Clear();
         isEnded = false;
     }
-
     public void GameEnded(){
         foreach(Letter letter in letters){
             letter.Fall();
         }
 
         Debug.Log("Game Ended");
-        numberofLetters = 0;
+        numberofSpaces = 0;
 
         isEnded = true;
     }
